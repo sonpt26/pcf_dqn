34f71f47cbf28a396a33b9576be7d699b82ee734
vandertyson <vandertyson117@gmail.com>
2024-05-10 09:45:15 +0000

    revenue calculate

--- network.py
+++ network.py
@@ -6,6 +6,7 @@ import threading
 import time
 from atomiclong import AtomicLong
 import timeit
+from queue import Empty
 
 
 class Packet:
@@ -29,29 +30,27 @@ class NetworkEnv(gym.Env):
         # Parameters
         self.scale_factor = 1
         self.generator_setting = {
-            "TF1": {"num_thread": 2, "packet_size": 1024, "rate": 10, "price": 1},
-            "TF2": {"num_thread": 2, "packet_size": 1024, "rate": 20, "price": 2},
-            "TF3": {"num_thread": 1, "packet_size": 10240, "rate": 10, "price": 3},
+            "TF1": {"num_thread": 2, "packet_size": 1024, "rate": 10, "price": 30},
+            "TF2": {"num_thread": 2, "packet_size": 1024, "rate": 20, "price": 20},
+            "TF3": {"num_thread": 1, "packet_size": 10240, "rate": 100, "price": 3},
         }
         self.processor_setting = {
-            "NR": {"num_thread": 3, "limit": 200, "rate": 400, "revenue_factor": 1},
+            "NR": {"num_thread": 1, "limit": 200, "rate": 100, "revenue_factor": 1},
             "WF": {"num_thread": 1, "limit": 200, "rate": 100, "revenue_factor": 0},
         }
-        self.packet_generation_interval = 5  # seconds
-        self.total_simulation_time = 3600  # seconds
+        self.timeout_processor = 0.5
+        self.total_simulation_time = 30  # seconds
         self.traffic_classes = list(self.generator_setting.keys())
         self.choices = list(self.processor_setting.keys())
         self.action_space = spaces.Box(
             low=0, high=1, shape=(len(self.traffic_classes),), dtype=np.float32
         )
         self.queue = {}
-        self.queue_limit = {}
         for key, value in self.processor_setting.items():
             self.queue[key] = queue.Queue(value["limit"] * value["num_thread"])
 
         self.observation_space = spaces.Box(low=0, high=100, dtype=np.float32)
         self.total_revenue = AtomicLong(0)
-        self.current_time = 0
 
     def packet_generator(self, traffic_class, action):
         setting = self.generator_setting[traffic_class]
@@ -69,35 +68,45 @@ class NetworkEnv(gym.Env):
             queue = self.queue[choice]
             if queue.full():
                 accum_counter["drop"] += 1
+                self.stat[choice][traffic_class]["loss"] += 1
             else:
                 packet = Packet(time.time_ns(), traffic_class)
                 queue.put(packet)
                 accum_counter[choice] += 1
             timeit.time.sleep(time_to_wait)
             if time.time() - start_time > self.total_simulation_time:
+                print("Generator finish", traffic_class)
                 break
 
     def packet_processor(self, tech, rate, queue):
         start = time.time()
         print("Processor", tech, rate, "mbps")
         while True:
-            item = queue.get()
-            if item is None:
-                time.sleep(0.0001)
-                continue
-            traffic_class = item.get_traffic_class()
-            packet_size = self.generator_setting[traffic_class]["packet_size"]
-            process_time = packet_size * 1.0 * 8 / (rate * 1e6)
-            timeit.time.sleep(process_time)
-            latency = time.time_ns() - item.get_start()
-            if latency <= 0:
-                print("Negative time", latency)
-            else:
-                self.accumulators[traffic_class]["latency"].append(latency)
-                self.stat[tech][traffic_class]["revenue"] += 1
-                self.stat[tech][traffic_class]["packet_count"] += 1
-            if time.time() - start > self.total_simulation_time:
-                break
+            try:
+                item = queue.get(timeout=self.timeout_processor)
+                if item is None:
+                    time.sleep(0.0001)
+                    continue
+                else:
+                    traffic_class = item.get_traffic_class()
+                    packet_size = self.generator_setting[traffic_class]["packet_size"]
+                    process_time = packet_size * 1.0 * 8 / (rate * 1e6)
+                    timeit.time.sleep(process_time)
+                    latency = time.time_ns() - item.get_start()
+                    if latency <= 0:
+                        print("Negative time", latency)
+                    else:
+                        self.accumulators[traffic_class]["latency"].append(latency)
+                        self.stat[tech][traffic_class]["revenue"] += 1
+                        self.stat[tech][traffic_class]["packet_count"] += 1
+                    queue.task_done()
+            except Exception as error:
+                if type(error) is Empty:
+                    if time.time() - start > self.total_simulation_time + 2:
+                        print("Processor finish ", tech)
+                        break
+                    continue
+                print("Exception", error)
 
     def print_stat(self):
         start = time.time()
@@ -130,6 +139,7 @@ class NetworkEnv(gym.Env):
             for key, value in self.stat.items():
                 log_str = key
                 total_revenue = 0
+                total_loss = 0
                 total_data = 0
                 rev_factor = self.processor_setting[key]["revenue_factor"]
                 for k, v in value.items():
@@ -140,14 +150,22 @@ class NetworkEnv(gym.Env):
                         / 8
                         / 1e6
                     )
-                    total_revenue += tf_rev
+                    tf_loss = (
+                        self.generator_setting[k]["price"]
+                        * self.generator_setting[k]["packet_size"]
+                        * v["loss"].value
+                        / 8
+                        / 1e6
+                    )
+                    total_revenue += self.scale_factor * tf_rev
+                    total_loss += self.scale_factor * tf_loss
                     total_data += self.scale_factor * (
                         v["packet_count"].value
                         * self.generator_setting[k]["packet_size"]
                         * 8
                     )
                     throughput = self.scale_factor * (
-                        v['packet_count'].value
+                        v["packet_count"].value
                         * self.generator_setting[k]["packet_size"]
                         * 8
                         / (5 * 1e6)
@@ -155,27 +173,41 @@ class NetworkEnv(gym.Env):
                     log_str += (
                         ". "
                         + k
-                        + ": "
+                        + ". R: "
                         + str(round(tf_rev, 2))
-                        + "$/"
+                        + "$. L: "
+                        + str(round(tf_loss, 2))
+                        + "$. T: "
                         + str(round(throughput, 2))
                         + " mbps"
                     )
+                    v["packet_count"].value = 0
                 log_str += (
-                    ". Total: " + str(round(total_revenue * rev_factor, 2)) + "$/"+ str(round(total_data / (5 * 1e6), 2)) + " mbps"
-                )                
+                    "|All. R: "
+                    + str(round(total_revenue * rev_factor, 2))
+                    + "$. L: "
+                    + str(round(total_loss * rev_factor, 2))
+                    + "$. T: "
+                    + str(round(total_data / (5 * 1e6), 2))
+                    + " mbps"
+                )
                 print(log_str)
+                longest = max(longest, len(log_str))
 
             separator = "=" * longest
             print(separator)
             start_interval = time.time()
-            if time.time() - start > self.total_simulation_time:
+            if time.time() - start > (self.total_simulation_time):
+                print("Stop monitor")
                 break
 
     def step(self, action):
+        start_step = time.time()
         self.generators = {}
         self.accumulators = {}
         self.stat = {}
+        list_generator_threads = []
+        list_processor_threads = []
         for key, value in self.processor_setting.items():
             my_queue = self.queue[key]
             self.stat[key] = {}
@@ -188,11 +220,13 @@ class NetworkEnv(gym.Env):
                         my_queue,
                     ),
                 )
-                processor.start()
+                list_processor_threads.append(processor)
+
             for tf in self.generator_setting.keys():
                 self.stat[key][tf] = {
                     "revenue": AtomicLong(0),
                     "packet_count": AtomicLong(0),
+                    "loss": AtomicLong(0),
                 }
 
         for key, value in self.generator_setting.items():
@@ -212,16 +246,41 @@ class NetworkEnv(gym.Env):
                         action,
                     ),
                 )
-                packet_generator_thread.start()
+                list_generator_threads.append(packet_generator_thread)
+
+        for t in list_processor_threads:
+            t.start()
+
+        for t in list_generator_threads:
+            t.start()
 
         log_thread = threading.Thread(target=self.print_stat)
         log_thread.start()
+
+        for t in list_processor_threads:
+            t.join()
+
+        for t in list_generator_threads:
+            t.join()
+
+        log_thread.join()
+        result = ", ".join(
+            map(lambda kv: f"{kv[0]}: {kv[1].qsize()}", self.queue.items())
+        )
+        print(
+            "Finish step. Queue {" + result + "}.",
+            "Total time:",
+            str(round(time.time() - start_step, 2)),
+            "s",
+        )
         return [], 0, False, {}
 
     def reset(self):
+        print("Reset not implemented")
         pass
 
     def render(self, mode="human"):
+        print("Render not implemented")
         pass
 
     def close(self):
@@ -230,7 +289,6 @@ class NetworkEnv(gym.Env):
 
 env = NetworkEnv()
 observation = env.reset()
-action = env.action_space.sample()
-# action = [1,1,1,1]
-# action = [1, 1, 1]
+# action = env.action_space.sample()
+action = [0.1, 0, 1]
 observation, reward, done, _ = env.step(action)
